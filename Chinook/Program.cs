adbe38714d434749189103e41d174c12af8f18bb
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-02-24 14:04:54 +0100

    initialize pair programming!!!

--- Chinook/Program.cs
+++ Chinook/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Chinook
+﻿using Microsoft.Data.SqlClient;
+
+namespace Chinook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "N-NO-01-01-1451\\SQLEXPRESS";
+            builder.IntegratedSecurity= true;
+            builder.InitialCatalog = "Chinook";
+            builder.TrustServerCertificate= true;
+            Console.WriteLine(builder.ConnectionString);
         }
     }
 }
